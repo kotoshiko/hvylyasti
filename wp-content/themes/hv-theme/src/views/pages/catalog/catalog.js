9371bb810b5dcf152b749a19c443bc4b520f609b
kotoshiko <sofd005@gmail.com>
2024-10-21 23:54:01 +0300

    add new theme and front files

---
+++ wp-content/themes/hv-theme/src/views/pages/catalog/catalog.js
@@ -0,0 +1,28 @@
+document.querySelectorAll(".filter-content").forEach(function (content) {
+  content.style.height = content.scrollHeight + "px";
+});
+
+document.querySelectorAll(".filter-header").forEach(function (header) {
+  header.addEventListener("click", function () {
+    const contents = header.parentNode.querySelectorAll(".filter-content");
+
+    contents.forEach(function (content) {
+      if (content.style.height === "0px" || content.style.height === "") {
+        content.style.height = content.scrollHeight + "px";
+        header.classList.add("active");
+
+        setTimeout(function () {
+          content.style.height = "auto";
+        }, 300);
+      } else {
+        content.style.height = content.scrollHeight + "px";
+
+        setTimeout(function () {
+          content.style.height = "0";
+        }, 10);
+
+        header.classList.remove("active");
+      }
+    });
+  });
+});
