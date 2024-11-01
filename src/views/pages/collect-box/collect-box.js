73f71207ad4ea1f44c80f22f2499ae56512cc3ae
Kyrylo Rusakov <Kyrylo_Rusakov@epam.com>
2024-11-01 14:54:07 +0200

    add modal for composition product

--- src/views/pages/collect-box/collect-box.js
+++ src/views/pages/collect-box/collect-box.js
@@ -67,3 +67,73 @@ closeBtn.addEventListener('click', closeFullscreen);
 fullscreenImage.addEventListener('click', (event) => {
   event.stopPropagation();
 });
+
+//Modal
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.querySelector('.button-sum');
+  const modal = document.getElementById('productModal');
+  const closeButton = document.querySelector('.close-btn');
+
+  button.addEventListener('click', function(event) {
+    event.preventDefault();
+    modal.style.display = 'flex';
+  });
+
+  closeButton.addEventListener('click', function() {
+    modal.style.display = 'none';
+  });
+
+  modal.addEventListener('click', function(event) {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+});
+
+//Modal accordion
+document.addEventListener('DOMContentLoaded', function () {
+  const tasteElements = document.querySelectorAll('.product-taste');
+  const compositionElements = document.querySelectorAll('.product-composition');
+
+  tasteElements.forEach((tasteElement, index) => {
+    tasteElement.addEventListener('click', function () {
+      const currentCompositionElement = compositionElements[index];
+
+      compositionElements.forEach((compositionElement, compIndex) => {
+        if (compIndex !== index && compositionElement.classList.contains('open')) {
+          compositionElement.style.height = `${compositionElement.scrollHeight}px`;
+          requestAnimationFrame(() => {
+            compositionElement.style.height = '0';
+          });
+          compositionElement.classList.remove('open');
+        }
+      });
+
+      tasteElement.classList.toggle('open');
+
+      if (currentCompositionElement.classList.contains('open')) {
+        currentCompositionElement.style.height = `${currentCompositionElement.scrollHeight}px`;
+        requestAnimationFrame(() => {
+          currentCompositionElement.style.height = '0';
+        });
+        currentCompositionElement.classList.remove('open');
+      } else {
+        currentCompositionElement.style.height = '0';
+        currentCompositionElement.classList.add('open');
+        requestAnimationFrame(() => {
+          currentCompositionElement.style.height = `${currentCompositionElement.scrollHeight}px`;
+        });
+      }
+    });
+  });
+
+  compositionElements.forEach((compositionElement) => {
+    compositionElement.addEventListener('transitionend', function () {
+      if (!compositionElement.classList.contains('open')) {
+        compositionElement.style.height = '';
+      } else {
+        compositionElement.style.height = 'auto';
+      }
+    });
+  });
+});
