0349a01a9a2f18d3cdf61fbd5a116f34c7d8c751
NIX <nix@macair.home>
2024-11-07 14:18:46 +0100

    added partners and delivery pages

---
+++ wp-content/themes/hv-theme/parts/content-delivery.php
@@ -0,0 +1,48 @@
+<?php
+?>
+<section class="delivery">
+  <div class="title-section">
+    <img src="/wp-content/themes/hv-theme/assets/images/bg-wave.png" class="bg-wave" alt="" />
+    <div class="title-section-inner">
+      <h1 class="title title-big">Доставка та оплата</h1>
+    </div>
+  </div>
+  <div class="container flex-column center-x">
+    <div class="delivery-inner">
+      <div class="delivery-content">
+        <div class="delivery-content-banner">
+          <img src="/wp-content/themes/hv-theme/assets/images/delivery/delivery-1.png" alt="logo" />
+        </div>
+        <div class="delivery-info">
+          <h3 class="delivery-title">Про нас</h3>
+          <p class="delivery-text">
+            Ми подбали про те, щоб доставка замовлення була максимально простою та зручною. Ми здійснюємо доставку Новою
+            Поштою та Укрпоштою. Відправлення відбувається кожного буднього дня, окрім вихідних, тобто суботи і
+            неділі.
+          </p>
+          <p class="delivery-text">Ваше замовлення поїде до вас протягом тижня після замовлення.</p>
+          <p class="delivery-text">У разі, якщо ви не забрали замовлення впродовж 5 днів, ми робимо повернення.</p>
+        </div>
+      </div>
+    </div>
+    <div class="delivery-inner">
+      <div class="delivery-content">
+        <div class="delivery-info">
+          <h3 class="delivery-title">Способи оплати</h3>
+          <p class="delivery-text">
+            Оплатити можна картою (Visa, Mastercard)або Privat24 через LiqPay або внести післяплату на пошті при
+            отриманні. Після замовлення вам на пошту прийде лист із деталями Вашого замовлення та додатковою
+            інформацією.
+          </p>
+          <div class="delivery-icons">
+            <img src="/wp-content/themes/hv-theme/assets/images/delivery/visa.png" alt="visa">
+            <img src="/wp-content/themes/hv-theme/assets/images/delivery/mastercard.png" alt="mastercard">
+          </div>
+        </div>
+        <div class="delivery-content-banner">
+          <img src="/wp-content/themes/hv-theme/assets/images/delivery/delivery-2.png" alt="logo" />
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
