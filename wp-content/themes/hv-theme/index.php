94bfc4c438e403e0033a3b9062afdff229f4b733
NIX <nix@macair.home>
2024-11-07 01:42:44 +0100

    created welcome-section

--- wp-content/themes/hv-theme/index.php
+++ wp-content/themes/hv-theme/index.php
@@ -2,9 +2,53 @@
 get_header();
 ?>
 <main class="main-container">
-	<div class="main-content">
-		<?php get_template_part('parts/content', 'home')?>
-	</div>
+<div class="home">
+  <section class="welcome-section">
+    <div class="welcome-section-content">
+      <div class="welcome-section-info">
+        <div class="welcome-section-text">
+          <h1 class="title welcome-section-title">Збери свій <br> ящик смаків</h1>
+          <div class="welcome-section-icons">
+            <img src="/wp-content/themes/hv-theme/assets/images/home/welcome-icon.svg" alt="icon">
+            <p>12+ смаків на вибір</p>
+          </div>
+          <div class="welcome-section-action">
+            <a href="/" class="button button-solid">Купити
+              <span class="icon-arrow">
+                <img src="/wp-content/themes/hv-theme/assets/images/icons/button-arrow.svg" alt="">
+              </span>
+            </a>
+          </div>
+        </div>
+
+      </div>
+      <div class="welcome-section-video">
+        <video src="/wp-content/themes/hv-theme/assets/videos/main_comp.mp4" autoplay loop muted></video>
+      </div>
+    </div>
+    <img src="/wp-content/themes/hv-theme/assets/images/home/home_white_wawe.svg" class="welcome-section-white-wave" alt="" />
+  </section>
+
+  <div class="properties-section">
+    <div class="main-content">
+      <div class="properties-section-container">
+      </div>
+    </div>
+  </div>
+  <div class="trends-section">
+    <div class="main-content">
+      <div class="properties-section-container">
+      </div>
+    </div>
+  </div>
+  <div class="social-marketing-section">
+    <div class="main-content">
+      <div class="properties-section-container">
+      </div>
+    </div>
+  </div>
+</div>
+
 </main>
 <?php get_footer();?>
 </body>
