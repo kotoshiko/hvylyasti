71eba282ad5a6a8532f6a7aca0f9afb06f1b0ffb
NIX <nix@macair.home>
2024-11-08 15:57:33 +0100

    update-home-page

--- wp-content/themes/hv-theme/index.php
+++ wp-content/themes/hv-theme/index.php
@@ -3,10 +3,11 @@ get_header();
 ?>
 <main class="main-container">
   <div class="home">
-    <section class="welcome-section">
+    <div class="welcome-section">
       <div class="welcome-section-content">
         <div class="welcome-section-info">
-          <img src="/wp-content/themes/hv-theme/assets/images/home/welcome-shape.svg" class="welcome-section-shape" alt="icon">
+          <img src="/wp-content/themes/hv-theme/assets/images/home/welcome-shape.svg" class="welcome-section-shape"
+            alt="icon">
           <div class="welcome-section-inner">
             <div class="welcome-section-text">
               <h1 class="title welcome-section-title">Збери свій <br> ящик смаків</h1>
@@ -23,32 +24,66 @@ get_header();
               </div>
             </div>
           </div>
-          <img src="/wp-content/themes/hv-theme/assets/images/home/home_white_wawe.svg" class="welcome-section-white-wave" alt="" />
+          <img src="/wp-content/themes/hv-theme/assets/images/home/home_white_wawe.svg"
+            class="welcome-section-white-wave" alt="" />
         </div>
         <div class="welcome-section-video">
           <video src="/wp-content/themes/hv-theme/assets/videos/main_comp.mp4" autoplay loop muted></video>
         </div>
       </div>
-    </section>
+    </div>
 
     <div class="properties-section">
-      <div class="main-content">
+      <div class="home-content main-content">
+        <h2 class="title">Грінки Хвилясті</h2>
         <div class="properties-section-container">
+          <img class="home-image" src="/wp-content/themes/hv-theme/assets/images/benefit.png">
         </div>
       </div>
     </div>
+
     <div class="trends-section">
-      <div class="main-content">
-        <div class="properties-section-container">
+      <img src="/wp-content/themes/hv-theme/assets/images/home/section_white_wawe.svg" class="section-white-wawe"
+        alt="wawe">
+      <div class="home-content main-content">
+        <h2 class="title">Тренди</h2>
+        <div class="trends-section-container">
+          <img class="home-image" src="/wp-content/themes/hv-theme/assets/images/benefit.png">
         </div>
+        <a href="/" class="button button-solid-secondary">Скласти власний бокс</a>
       </div>
     </div>
+
     <div class="social-marketing-section">
-      <div class="main-content">
-        <div class="properties-section-container">
+      <img src="/wp-content/themes/hv-theme/assets/images/home/section_grey_wawe.svg" class="section-white-wawe"
+        alt="wawe">
+      <div class="home-content main-content">
+        <h2 class="title">Про нас</h2>
+        <p class="home-section-text">Свіжий житньо-пшеничний хліб, унікальна форма, приправи, що виготовлені на
+          натуральній основі або натуральних ароматизаторах, а також створення продукції ручним способом — усе це
+          вирізняє Хвилясті серед інших.</p>
+        <div class="social-marketing-section-container">
+          <img class="home-image" src="/wp-content/themes/hv-theme/assets/images/benefit.png">
         </div>
+        <a href="/" class="button button-outlined">Дізнатися більше</a>
       </div>
     </div>
+
+    <div class="video-section">
+      <img src="/wp-content/themes/hv-theme/assets/images/home/section_white_wawe.svg" class="section-white-wawe"
+        alt="wawe">
+      <div class="home-content main-content">
+        <h2 class="title">Для партнерів</h2>
+        <p class="home-section-text">Наш продукт є ваговим. Кожен ящик містить 12 порцій, розташованих в два яруси.
+          В одній порції 7 – 12 грінок, вага яких 75 г, в залежності від розміру хліба. Продукція ручної праці дає
+          можливість отримати продукт найвищої якості в широкому асортименті.</p>
+        <div class="video-section-container">
+          <video src="/wp-content/themes/hv-theme/assets/videos/main_comp.mp4" autoplay loop muted controls></video>
+        </div>
+        <a href="/" class="button button-solid">Заявка на співпрацю</a>
+      </div>
+    </div>
+
   </div>
 
 </main>
