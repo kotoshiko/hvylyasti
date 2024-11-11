0d7d0fb83686e465cdeadeeb744758bc44cce8d3
kotoshiko <sofd005@gmail.com>
2024-11-11 23:18:02 +0200

    add procesed to checkout functions to cart

--- wp-content/themes/hv-theme/woocommerce/cart/proceed-to-checkout-button.php
+++ wp-content/themes/hv-theme/woocommerce/cart/proceed-to-checkout-button.php
@@ -22,6 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<a href="<?php echo esc_url( wc_get_checkout_url() ); ?>" class="checkout-button button alt wc-forward<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>">
-	<?php esc_html_e( 'Proceed to checkout', 'woocommerce' ); ?>
+<a href="<?php echo esc_url( wc_get_checkout_url() ); ?>" class=" button button-solid-secondary checkout-button button alt wc-forward<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>">
+	<?php esc_html_e( 'Замовити', 'woocommerce' ); ?>
 </a>
+<a href="/shop" class="button button-outlined">Продовжити покупки</a>
